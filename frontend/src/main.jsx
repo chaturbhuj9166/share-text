5fb7c69e9812b3811f45f9f63c238596c859d5d6
Prem <joshichaturbhuj2@gmail.com>
2026-02-10 14:12:16 -0500

    first

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -6,10 +6,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-   <React.StrictMode>
+  
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+ 
 
 );
